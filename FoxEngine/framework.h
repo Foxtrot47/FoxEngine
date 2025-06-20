1d95ba862165d061c8454655450ce02cfb531c4f
foxtrot47 <jjneutron@outlook.com>
2025-06-20 22:56:22 +0530

    fix: exclude unused stuff from windows sdk

--- FoxEngine/framework.h
+++ FoxEngine/framework.h
@@ -5,7 +5,26 @@
 #pragma once
 
 #include "targetver.h"
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
 // Windows Header Files
 #include <windows.h>
 // C RunTime Header Files
