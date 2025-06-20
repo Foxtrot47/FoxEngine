156a71be81aaa10ee17313f5c367a4fbc63b7807
foxtrot47 <jjneutron@outlook.com>
2025-06-20 21:01:53 +0530

    Add project files.

---
+++ FoxEngine/FoxEngine.cpp
@@ -0,0 +1,5 @@
+// FoxEngine.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "FoxEngine.h"
---
+++ FoxEngine/resource.h
@@ -0,0 +1,11 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FoxEngine.rc
+//
+#define IDC_MYICON                      2
+#define IDS_APP_TITLE                   103
+#define IDI_FOXENGINE                   107
+#define IDI_SMALL                       108
+#define IDC_FOXENGINE                   109
+#define IDC_STATIC                      -1
+
