8373fff9a118d78a44613b9db8dbf62d49c77fc1
foxtrot47 <jjneutron@outlook.com>
2025-06-20 21:20:57 +0530

    feat: output hello world to console

--- FoxEngine/FoxEngine.cpp
+++ FoxEngine/FoxEngine.cpp
@@ -1,5 +1,16 @@
-// FoxEngine.cpp : Defines the entry point for the application.
-//
+// FoxEngine.cpp : Defines the entry point for the application.  
+//  
 
-#include "framework.h"
-#include "FoxEngine.h"
+#include "framework.h"  
+#include "FoxEngine.h"  
+
+int CALLBACK WinMain(  
+   _In_ HINSTANCE hInstance,  
+   _In_opt_ HINSTANCE hPrevInstance,  
+   _In_ LPSTR lpCmdLine,  
+   _In_ int nCmdShow  
+)  
+{  
+   OutputDebugStringA("Hello world");  
+   return 0;  
+}
