4d9853f825dfee989995d9f416b12d32b9e9d189
foxtrot47 <jjneutron@outlook.com>
2025-07-26 23:04:46 +0530

    feat: implement depth buffering

--- FoxEngine/Graphics.cpp
+++ FoxEngine/Graphics.cpp
@@ -17,7 +17,7 @@ Graphics::Graphics(HWND hWnd) : pDevice(nullptr), pSwapChain(nullptr), pContext(
 	scd.BufferDesc.Height = 0;                             // Set the back buffer height
 	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;     // How the swap chain is to be used
 	scd.OutputWindow = hWnd;                               // The window to be used
-	scd.SampleDesc.Count = 4;                              // How many multisamples
+	scd.SampleDesc.Count = 1;                              // No multisampling
 	scd.Windowed = TRUE;                                   // Windowed/full-screen mode
 	scd.Flags = 0;                                         // No special flags
 	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;             // Discard the old back buffer when presenting
@@ -46,6 +46,45 @@ Graphics::Graphics(HWND hWnd) : pDevice(nullptr), pSwapChain(nullptr), pContext(
 			pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pTarget); // Create a render target view  
 		}
 	}
+
+	D3D11_DEPTH_STENCIL_DESC descDepthStencil;
+	ZeroMemory(&descDepthStencil, sizeof(D3D11_DEPTH_STENCIL_DESC));
+	descDepthStencil.DepthEnable = TRUE;
+	descDepthStencil.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	descDepthStencil.DepthFunc = D3D11_COMPARISON_LESS;
+
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> pDepthStencilState;
+	HRESULT hr = pDevice->CreateDepthStencilState(&descDepthStencil, &pDepthStencilState);
+	pContext->OMSetDepthStencilState(pDepthStencilState.Get(), 1u); // Set the depth stencil state
+
+	// create depth stencil texture
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> pDepthStencilTexture;
+	D3D11_TEXTURE2D_DESC descDepth;
+	ZeroMemory(&descDepth, sizeof(D3D11_TEXTURE2D_DESC));
+	descDepth.Width = 1920; // Set the width of the depth stencil buffer
+	descDepth.Height = 1080; // Set the height of the depth stencil buffer
+	descDepth.MipLevels = 1u; // No mipmaps
+	descDepth.ArraySize = 1u; // Single texture
+	descDepth.Format = DXGI_FORMAT_D32_FLOAT; // 32-bit depth buffer
+	descDepth.SampleDesc.Count = 1; // No multisampling
+	descDepth.SampleDesc.Quality = 0u; // No multisampling quality
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL; // Bind as a depth stencil buffer
+	pDevice->CreateTexture2D(&descDepth, nullptr, &pDepthStencilTexture);	// Create the depth stencil buffer
+
+	// create view of the depth stencil texture
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDepthStencilView;
+	ZeroMemory(&descDepthStencilView, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	descDepthStencilView.Format = DXGI_FORMAT_D32_FLOAT;
+	descDepthStencilView.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDepthStencilView.Texture2D.MipSlice = 0; // Use the first mip level
+	pDevice->CreateDepthStencilView(
+		pDepthStencilTexture.Get(), 
+		&descDepthStencilView,
+		&pDepthStencilView
+	);
+
+	pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), pDepthStencilView.Get()); // Set the render target and depth stencil view
 }
 
 void Graphics::EndFrame()
@@ -62,6 +101,12 @@ void Graphics::ClearBuffer(float red, float green, float blue)
 	if (pContext) {
 		const float clearColor[4] = { red, green, blue, 1.0f }; // RGBA color
 		pContext->ClearRenderTargetView(pTarget.Get(), clearColor); // Clear the render target view
+		pContext->ClearDepthStencilView(
+			pDepthStencilView.Get(),
+			D3D11_CLEAR_DEPTH, // Clear depth and stencil
+			1.0f, // Depth value to clear to
+			0u // Stencil value to clear to
+		); // Clear the depth stencil view
 	}
 }
 
@@ -248,8 +293,6 @@ void Graphics::DrawCube(float angle, float x, float y)
 	);
 
 	pContext->IASetInputLayout(pInputLayout.Get());						// Set the input layout
-
-	pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr); // Set the render target
 
 	D3D11_VIEWPORT vp;
 	ZeroMemory(&vp, sizeof(D3D11_VIEWPORT));
