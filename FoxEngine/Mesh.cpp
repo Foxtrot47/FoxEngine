ec22b864c466e32d6fa123d368bdbec789e7d3ff
foxtrot47 <jjneutron@outlook.com>
2025-08-16 21:09:36 +0530

    fix: do not use static bindable - this was stupid from the start, you need to have separate buffer data for diffrent bindables. static is not the way to go.

--- FoxEngine/Mesh.cpp
+++ FoxEngine/Mesh.cpp
@@ -4,35 +4,59 @@
 
 Mesh::Mesh(Graphics& gfx, std::vector<Vertex> vertices, std::vector<unsigned short> indices, std::wstring texturePath)
 {
-	if (!IsStaticInitialized())
+	//if (!IsStaticInitialized())
+	//{
+	//	AddStaticBindable(std::make_unique<VertexBuffer>(gfx, vertices));
+
+	//	AddStaticBindable(std::make_unique<Texture>(gfx, texturePath));
+
+	//	auto pVertexShader = std::make_unique<VertexShader>(gfx, GetExecutableDirectory() + L"\\PhongVS.cso");
+	//	auto pVertexShaderByteCode = pVertexShader->GetByteCode();
+
+	//	AddStaticBindable(std::move(pVertexShader));
+
+	//	AddStaticBindable(std::make_unique<PixelShader>(gfx, GetExecutableDirectory() + L"\\PhongPS.cso"));
+
+	//	AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, indices));
+
+	//	const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
+	//	{
+	//		{ "Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	//		{ "Normal", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	//		{ "TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	//	};
+
+	//	AddStaticBindable(std::make_unique<InputLayout>(gfx, ied, pVertexShaderByteCode));
+
+	//	AddStaticBindable(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+	//}
+	//else {
+	//	SetIndexBufferFromStatic();
+	//}
+
+	AddBind(std::make_unique<VertexBuffer>(gfx, vertices));
+
+	AddBind(std::make_unique<Texture>(gfx, texturePath));
+
+	auto pVertexShader = std::make_unique<VertexShader>(gfx, GetExecutableDirectory() + L"\\PhongVS.cso");
+	auto pVertexShaderByteCode = pVertexShader->GetByteCode();
+
+	AddBind(std::move(pVertexShader));
+
+	AddBind(std::make_unique<PixelShader>(gfx, GetExecutableDirectory() + L"\\PhongPS.cso"));
+
+	AddIndexBuffer(std::make_unique<IndexBuffer>(gfx, indices));
+
+	const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
 	{
-		AddStaticBindable(std::make_unique<VertexBuffer>(gfx, vertices));
+		{ "Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "Normal", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
 
-		AddStaticBindable(std::make_unique<Texture>(gfx, texturePath));
+	AddBind(std::make_unique<InputLayout>(gfx, ied, pVertexShaderByteCode));
 
-		auto pVertexShader = std::make_unique<VertexShader>(gfx, GetExecutableDirectory() + L"\\PhongVS.cso");
-		auto pVertexShaderByteCode = pVertexShader->GetByteCode();
-
-		AddStaticBindable(std::move(pVertexShader));
-
-		AddStaticBindable(std::make_unique<PixelShader>(gfx, GetExecutableDirectory() + L"\\PhongPS.cso"));
-
-		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, indices));
-
-		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
-		{
-			{ "Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "Normal", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
-		};
-
-		AddStaticBindable(std::make_unique<InputLayout>(gfx, ied, pVertexShaderByteCode));
-
-		AddStaticBindable(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
-	}
-	else {
-		SetIndexBufferFromStatic();
-	}
+	AddBind(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
 
 	AddBind(std::make_unique<TransformConstantBuffer>(gfx));
 }
