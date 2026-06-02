Foxtrot47/slopengiine

M12: Mesh class + Assimp FBX import; mossy stone wall in test scene
- Add vcpkg manifest (assimp 6.0.4); CMAKE_PREFIX_PATH wired for manifest mode
- Mesh/VertexBuffer/IndexBuffer abstraction; MeshVertex = pos+normal+uv (32 B)
- Texture2D (WIC), SamplerState (FilterMode/AddressMode enums), ConstantBuffer<T>
- Depth/stencil buffer (D24_UNORM_S8_UINT); Z-test enabled
- Git LFS tracking for binary assets; removed legacy Assets/Shaders

Game/src/main.cpp
@@ -5,20 +5,13 @@
 #include <DirectXMath.h>
 #include "Engine/Core/Engine.h"
 #include "Engine/Core/Logger.h"
-#include "Engine/Renderer/VertexBuffer.h"
-#include "Engine/Renderer/IndexBuffer.h"
+#include "Engine/Renderer/Mesh.h"
 #include "Engine/Renderer/ConstantBuffer.h"
 #include "Engine/Renderer/Texture2D.h"
 #include "Engine/Renderer/SamplerState.h"
 
 using Microsoft::WRL::ComPtr;
 using namespace DirectX;
-
-struct Vertex
-{
-    float x, y, z;
-    float u, v;
-};
 
 struct TransformCB
 {
@@ -51,38 +44,32 @@ public:
         SE_HR(device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &m_vs));
         SE_HR(device->CreatePixelShader(psBlob->GetBufferPointer(),  psBlob->GetBufferSize(), nullptr, &m_ps));
 
+        // ---- Input layout — must match MeshVertex exactly ----
         D3D11_INPUT_ELEMENT_DESC layoutDesc[] =
         {
             { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
         };
-        SE_HR(device->CreateInputLayout(layoutDesc, 2,
+        SE_HR(device->CreateInputLayout(layoutDesc, 3,
             vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &m_layout));
 
-        // UVs go 0→2 so address mode differences are obvious.
-        Vertex verts[] =
-        {
-            { -0.5f,  0.5f, 0.0f,   0.0f, 0.0f },
-            {  0.5f,  0.5f, 0.0f,   2.0f, 0.0f },
-            { -0.5f, -0.5f, 0.0f,   0.0f, 2.0f },
-            {  0.5f, -0.5f, 0.0f,   2.0f, 2.0f },
-        };
-        uint32_t indices[] = { 0, 1, 2,   1, 3, 2 };
+        if (!m_transformCB.Create(device)) return false;
 
-        if (!m_vb.Create(device, verts, sizeof(verts), sizeof(Vertex))) return false;
-        if (!m_ib.Create(device, indices, 6))                           return false;
-        if (!m_transformCB.Create(device))                              return false;
-
-        if (!m_texture.LoadFromFile(device,
-            L"Assets/Textures/Stained_Concrete_Ceiling_ueydffrdy_1K_BaseColor.jpg"))
+        // ---- Load FBX mesh ----
+        if (!m_mesh.Load(device, "Assets/Meshes/Mossy_Stone_Wall_ukhgdfyga_Low.fbx"))
             return false;
 
-        // Three samplers to compare side by side.
-        if (!m_pointWrap.Create(device,   { SE::FilterMode::Point,      SE::AddressMode::Wrap  })) return false;
-        if (!m_linearWrap.Create(device,  { SE::FilterMode::Trilinear,  SE::AddressMode::Wrap  })) return false;
-        if (!m_linearClamp.Create(device, { SE::FilterMode::Trilinear,  SE::AddressMode::Clamp })) return false;
+        // ---- Load BaseColor texture ----
+        if (!m_texture.LoadFromFile(device,
+            L"Assets/Textures/Mossy_Stone_Wall_ukhgdfyga_Low_1K_BaseColor.jpg"))
+            return false;
 
-        SE_LOG_INFO("TestScene ready — sampler comparison");
+        // ---- Anisotropic sampler — this is the game-quality default ----
+        if (!m_sampler.Create(device, { SE::FilterMode::Anisotropic, SE::AddressMode::Wrap }))
+            return false;
+
+        SE_LOG_INFO("TestScene ready — mossy stone wall");
         return true;
     }
 
@@ -90,63 +77,50 @@ protected:
     void OnUpdate() override
     {
         ID3D11DeviceContext* ctx = GetRenderer().GetContext();
+        float t = static_cast<float>(GetClock().GetTotalTime());
 
-        XMMATRIX view = XMMatrixLookAtLH(
-            XMVectorSet(0.0f, 0.0f, -3.5f, 1.0f),
-            XMVectorSet(0.0f, 0.0f,  0.0f, 1.0f),
-            XMVectorSet(0.0f, 1.0f,  0.0f, 0.0f));
-        XMMATRIX proj = XMMatrixPerspectiveFovLH(
+        // Scale the mesh down — FAB assets often export at real-world cm scale.
+        XMMATRIX model = XMMatrixScaling(0.02f, 0.02f, 0.02f)
+                       * XMMatrixRotationY(t * 0.4f);
+        XMMATRIX view  = XMMatrixLookAtLH(
+            XMVectorSet(0.0f, 2.0f, -10.0f, 1.0f),
+            XMVectorSet(0.0f, 2.0f,   0.0f, 1.0f),
+            XMVectorSet(0.0f, 1.0f,   0.0f, 0.0f));
+        XMMATRIX proj  = XMMatrixPerspectiveFovLH(
             XMConvertToRadians(60.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
 
-        ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-        ctx->IASetInputLayout(m_layout.Get());
-        ctx->VSSetShader(m_vs.Get(), nullptr, 0);
-        ctx->PSSetShader(m_ps.Get(), nullptr, 0);
-        m_vb.Bind(ctx);
-        m_ib.Bind(ctx);
-        m_texture.BindPS(ctx, 0);
-
-        // Left — Point filter + Wrap (pixelated, tiling)
-        DrawQuad(ctx, XMMatrixTranslation(-1.3f, 0.0f, 0.0f), view, proj, m_pointWrap);
-
-        // Centre — Trilinear + Wrap (smooth, tiling)
-        DrawQuad(ctx, XMMatrixTranslation( 0.0f, 0.0f, 0.0f), view, proj, m_linearWrap);
-
-        // Right — Trilinear + Clamp (smooth, edge stretched)
-        DrawQuad(ctx, XMMatrixTranslation( 1.3f, 0.0f, 0.0f), view, proj, m_linearClamp);
-    }
-
-private:
-    void DrawQuad(ID3D11DeviceContext* ctx,
-                  XMMATRIX model, XMMATRIX view, XMMATRIX proj,
-                  const SE::SamplerState& sampler)
-    {
         TransformCB cb;
         XMStoreFloat4x4(&cb.model,      model);
         XMStoreFloat4x4(&cb.view,       view);
         XMStoreFloat4x4(&cb.projection, proj);
         m_transformCB.Update(ctx, cb);
         m_transformCB.BindVS(ctx, 0);
-        sampler.BindPS(ctx, 0);
-        ctx->DrawIndexed(m_ib.GetCount(), 0, 0);
+
+        m_texture.BindPS(ctx, 0);
+        m_sampler.BindPS(ctx, 0);
+
+        ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+        ctx->IASetInputLayout(m_layout.Get());
+        ctx->VSSetShader(m_vs.Get(), nullptr, 0);
+        ctx->PSSetShader(m_ps.Get(), nullptr, 0);
+
+        m_mesh.Draw(ctx);
     }
 
+private:
     ComPtr<ID3D11VertexShader>      m_vs;
     ComPtr<ID3D11PixelShader>       m_ps;
     ComPtr<ID3D11InputLayout>       m_layout;
-    SE::VertexBuffer                m_vb;
-    SE::IndexBuffer                 m_ib;
+    SE::Mesh                        m_mesh;
     SE::ConstantBuffer<TransformCB> m_transformCB;
     SE::Texture2D                   m_texture;
-    SE::SamplerState                m_pointWrap;
-    SE::SamplerState                m_linearWrap;
-    SE::SamplerState                m_linearClamp;
+    SE::SamplerState                m_sampler;
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     SE::WindowDesc desc;
-    desc.title  = L"SlopEngine — M11: Sampler States";
+    desc.title  = L"SlopEngine";
     desc.width  = 1280;
     desc.height = 720;
 

Foxtrot47/FoxEngine

Game/src/main.cpp
@@ -5,20 +5,13 @@
 #include <DirectXMath.h>
 #include "Engine/Core/Engine.h"
 #include "Engine/Core/Logger.h"
-#include "Engine/Renderer/VertexBuffer.h"
-#include "Engine/Renderer/IndexBuffer.h"
+#include "Engine/Renderer/Mesh.h"
 #include "Engine/Renderer/ConstantBuffer.h"
 #include "Engine/Renderer/Texture2D.h"
 #include "Engine/Renderer/SamplerState.h"
 
 using Microsoft::WRL::ComPtr;
 using namespace DirectX;
-
-struct Vertex
-{
-    float x, y, z;
-    float u, v;
-};
 
 struct TransformCB
 {
@@ -51,38 +44,32 @@ public:
         SE_HR(device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &m_vs));
         SE_HR(device->CreatePixelShader(psBlob->GetBufferPointer(),  psBlob->GetBufferSize(), nullptr, &m_ps));
 
+        // ---- Input layout — must match MeshVertex exactly ----
         D3D11_INPUT_ELEMENT_DESC layoutDesc[] =
         {
             { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
         };
-        SE_HR(device->CreateInputLayout(layoutDesc, 2,
+        SE_HR(device->CreateInputLayout(layoutDesc, 3,
             vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &m_layout));
 
-        // UVs go 0→2 so address mode differences are obvious.
-        Vertex verts[] =
-        {
-            { -0.5f,  0.5f, 0.0f,   0.0f, 0.0f },
-            {  0.5f,  0.5f, 0.0f,   2.0f, 0.0f },
-            { -0.5f, -0.5f, 0.0f,   0.0f, 2.0f },
-            {  0.5f, -0.5f, 0.0f,   2.0f, 2.0f },
-        };
-        uint32_t indices[] = { 0, 1, 2,   1, 3, 2 };
+        if (!m_transformCB.Create(device)) return false;
 
-        if (!m_vb.Create(device, verts, sizeof(verts), sizeof(Vertex))) return false;
-        if (!m_ib.Create(device, indices, 6))                           return false;
-        if (!m_transformCB.Create(device))                              return false;
-
-        if (!m_texture.LoadFromFile(device,
-            L"Assets/Textures/Stained_Concrete_Ceiling_ueydffrdy_1K_BaseColor.jpg"))
+        // ---- Load FBX mesh ----
+        if (!m_mesh.Load(device, "Assets/Meshes/Mossy_Stone_Wall_ukhgdfyga_Low.fbx"))
             return false;
 
-        // Three samplers to compare side by side.
-        if (!m_pointWrap.Create(device,   { SE::FilterMode::Point,      SE::AddressMode::Wrap  })) return false;
-        if (!m_linearWrap.Create(device,  { SE::FilterMode::Trilinear,  SE::AddressMode::Wrap  })) return false;
-        if (!m_linearClamp.Create(device, { SE::FilterMode::Trilinear,  SE::AddressMode::Clamp })) return false;
+        // ---- Load BaseColor texture ----
+        if (!m_texture.LoadFromFile(device,
+            L"Assets/Textures/Mossy_Stone_Wall_ukhgdfyga_Low_1K_BaseColor.jpg"))
+            return false;
 
-        SE_LOG_INFO("TestScene ready — sampler comparison");
+        // ---- Anisotropic sampler — this is the game-quality default ----
+        if (!m_sampler.Create(device, { SE::FilterMode::Anisotropic, SE::AddressMode::Wrap }))
+            return false;
+
+        SE_LOG_INFO("TestScene ready — mossy stone wall");
         return true;
     }
 
@@ -90,63 +77,50 @@ protected:
     void OnUpdate() override
     {
         ID3D11DeviceContext* ctx = GetRenderer().GetContext();
+        float t = static_cast<float>(GetClock().GetTotalTime());
 
-        XMMATRIX view = XMMatrixLookAtLH(
-            XMVectorSet(0.0f, 0.0f, -3.5f, 1.0f),
-            XMVectorSet(0.0f, 0.0f,  0.0f, 1.0f),
-            XMVectorSet(0.0f, 1.0f,  0.0f, 0.0f));
-        XMMATRIX proj = XMMatrixPerspectiveFovLH(
+        // Scale the mesh down — FAB assets often export at real-world cm scale.
+        XMMATRIX model = XMMatrixScaling(0.02f, 0.02f, 0.02f)
+                       * XMMatrixRotationY(t * 0.4f);
+        XMMATRIX view  = XMMatrixLookAtLH(
+            XMVectorSet(0.0f, 2.0f, -10.0f, 1.0f),
+            XMVectorSet(0.0f, 2.0f,   0.0f, 1.0f),
+            XMVectorSet(0.0f, 1.0f,   0.0f, 0.0f));
+        XMMATRIX proj  = XMMatrixPerspectiveFovLH(
             XMConvertToRadians(60.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
 
-        ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-        ctx->IASetInputLayout(m_layout.Get());
-        ctx->VSSetShader(m_vs.Get(), nullptr, 0);
-        ctx->PSSetShader(m_ps.Get(), nullptr, 0);
-        m_vb.Bind(ctx);
-        m_ib.Bind(ctx);
-        m_texture.BindPS(ctx, 0);
-
-        // Left — Point filter + Wrap (pixelated, tiling)
-        DrawQuad(ctx, XMMatrixTranslation(-1.3f, 0.0f, 0.0f), view, proj, m_pointWrap);
-
-        // Centre — Trilinear + Wrap (smooth, tiling)
-        DrawQuad(ctx, XMMatrixTranslation( 0.0f, 0.0f, 0.0f), view, proj, m_linearWrap);
-
-        // Right — Trilinear + Clamp (smooth, edge stretched)
-        DrawQuad(ctx, XMMatrixTranslation( 1.3f, 0.0f, 0.0f), view, proj, m_linearClamp);
-    }
-
-private:
-    void DrawQuad(ID3D11DeviceContext* ctx,
-                  XMMATRIX model, XMMATRIX view, XMMATRIX proj,
-                  const SE::SamplerState& sampler)
-    {
         TransformCB cb;
         XMStoreFloat4x4(&cb.model,      model);
         XMStoreFloat4x4(&cb.view,       view);
         XMStoreFloat4x4(&cb.projection, proj);
         m_transformCB.Update(ctx, cb);
         m_transformCB.BindVS(ctx, 0);
-        sampler.BindPS(ctx, 0);
-        ctx->DrawIndexed(m_ib.GetCount(), 0, 0);
+
+        m_texture.BindPS(ctx, 0);
+        m_sampler.BindPS(ctx, 0);
+
+        ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+        ctx->IASetInputLayout(m_layout.Get());
+        ctx->VSSetShader(m_vs.Get(), nullptr, 0);
+        ctx->PSSetShader(m_ps.Get(), nullptr, 0);
+
+        m_mesh.Draw(ctx);
     }
 
+private:
     ComPtr<ID3D11VertexShader>      m_vs;
     ComPtr<ID3D11PixelShader>       m_ps;
     ComPtr<ID3D11InputLayout>       m_layout;
-    SE::VertexBuffer                m_vb;
-    SE::IndexBuffer                 m_ib;
+    SE::Mesh                        m_mesh;
     SE::ConstantBuffer<TransformCB> m_transformCB;
     SE::Texture2D                   m_texture;
-    SE::SamplerState                m_pointWrap;
-    SE::SamplerState                m_linearWrap;
-    SE::SamplerState                m_linearClamp;
+    SE::SamplerState                m_sampler;
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     SE::WindowDesc desc;
-    desc.title  = L"FoxEngine — M11: Sampler States";
+    desc.title  = L"FoxEngine";
     desc.width  = 1280;
     desc.height = 720;
 