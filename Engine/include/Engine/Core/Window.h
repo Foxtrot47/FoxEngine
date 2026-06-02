Foxtrot47/slopengiine

M02: Win32 window + message loop; clean shutdown on close
Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

Engine/include/Engine/Core/Window.h
@@ -1,0 +1,40 @@
+#pragma once
+#include <windows.h>
+#include <cstdint>
+#include <string>
+
+namespace SE {
+
+struct WindowDesc
+{
+    std::wstring title  = L"SlopEngine";
+    uint32_t     width  = 1280;
+    uint32_t     height = 720;
+};
+
+class Window
+{
+public:
+    Window() = default;
+    ~Window();
+
+    bool Open(const WindowDesc& desc);
+    void Close();
+
+    // Returns false when the OS has posted WM_QUIT (user closed window).
+    bool PumpMessages();
+
+    HWND     GetHandle()  const { return m_hwnd; }
+    uint32_t GetWidth()   const { return m_width; }
+    uint32_t GetHeight()  const { return m_height; }
+    bool     IsOpen()     const { return m_hwnd != nullptr; }
+
+private:
+    static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+    HWND     m_hwnd   = nullptr;
+    uint32_t m_width  = 0;
+    uint32_t m_height = 0;
+};
+
+} // namespace SE

Engine/src/Core/Window.cpp
@@ -1,0 +1,87 @@
+#include "Engine/Core/Window.h"
+
+namespace SE {
+
+static const wchar_t* k_ClassName = L"SlopEngineWnd";
+
+Window::~Window()
+{
+    Close();
+}
+
+bool Window::Open(const WindowDesc& desc)
+{
+    m_width  = desc.width;
+    m_height = desc.height;
+
+    HINSTANCE hInst = GetModuleHandleW(nullptr);
+
+    WNDCLASSEXW wc   = {};
+    wc.cbSize        = sizeof(wc);
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInst;
+    wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOWFRAME);
+    wc.lpszClassName = k_ClassName;
+    RegisterClassExW(&wc);
+
+    // Calculate window size so the client area matches the requested resolution.
+    RECT rc = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    m_hwnd = CreateWindowExW(
+        0,
+        k_ClassName,
+        desc.title.c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, hInst, nullptr);
+
+    if (!m_hwnd)
+        return false;
+
+    ShowWindow(m_hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(m_hwnd);
+    return true;
+}
+
+void Window::Close()
+{
+    if (m_hwnd)
+    {
+        DestroyWindow(m_hwnd);
+        m_hwnd = nullptr;
+    }
+    UnregisterClassW(k_ClassName, GetModuleHandleW(nullptr));
+}
+
+bool Window::PumpMessages()
+{
+    MSG msg = {};
+    while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+    {
+        if (msg.message == WM_QUIT)
+            return false;
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+    return true;
+}
+
+LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        return 0;
+    }
+    return DefWindowProcW(hwnd, msg, wp, lp);
+}
+
+} // namespace SE

Game/src/main.cpp
@@ -4,11 +4,16 @@
 int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
                    LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
 {
-    SE::Engine engine;
+    SE::WindowDesc desc;
+    desc.title  = L"SlopEngine";
+    desc.width  = 1280;
+    desc.height = 720;
 
-    if (!engine.Initialize())
+    SE::Engine engine;
+    if (!engine.Initialize(desc))
         return 1;
 
+    engine.Run();
     engine.Shutdown();
     return 0;
 }

Foxtrot47/FoxEngine

Engine/include/Engine/Core/Window.h
@@ -1,0 +1,40 @@
+#pragma once
+#include <windows.h>
+#include <cstdint>
+#include <string>
+
+namespace SE {
+
+struct WindowDesc
+{
+    std::wstring title  = L"FoxEngine";
+    uint32_t     width  = 1280;
+    uint32_t     height = 720;
+};
+
+class Window
+{
+public:
+    Window() = default;
+    ~Window();
+
+    bool Open(const WindowDesc& desc);
+    void Close();
+
+    // Returns false when the OS has posted WM_QUIT (user closed window).
+    bool PumpMessages();
+
+    HWND     GetHandle()  const { return m_hwnd; }
+    uint32_t GetWidth()   const { return m_width; }
+    uint32_t GetHeight()  const { return m_height; }
+    bool     IsOpen()     const { return m_hwnd != nullptr; }
+
+private:
+    static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+    HWND     m_hwnd   = nullptr;
+    uint32_t m_width  = 0;
+    uint32_t m_height = 0;
+};
+
+} // namespace SE

Engine/src/Core/Window.cpp
@@ -1,0 +1,87 @@
+#include "Engine/Core/Window.h"
+
+namespace SE {
+
+static const wchar_t* k_ClassName = L"FoxEngineWnd";
+
+Window::~Window()
+{
+    Close();
+}
+
+bool Window::Open(const WindowDesc& desc)
+{
+    m_width  = desc.width;
+    m_height = desc.height;
+
+    HINSTANCE hInst = GetModuleHandleW(nullptr);
+
+    WNDCLASSEXW wc   = {};
+    wc.cbSize        = sizeof(wc);
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInst;
+    wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOWFRAME);
+    wc.lpszClassName = k_ClassName;
+    RegisterClassExW(&wc);
+
+    // Calculate window size so the client area matches the requested resolution.
+    RECT rc = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    m_hwnd = CreateWindowExW(
+        0,
+        k_ClassName,
+        desc.title.c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, hInst, nullptr);
+
+    if (!m_hwnd)
+        return false;
+
+    ShowWindow(m_hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(m_hwnd);
+    return true;
+}
+
+void Window::Close()
+{
+    if (m_hwnd)
+    {
+        DestroyWindow(m_hwnd);
+        m_hwnd = nullptr;
+    }
+    UnregisterClassW(k_ClassName, GetModuleHandleW(nullptr));
+}
+
+bool Window::PumpMessages()
+{
+    MSG msg = {};
+    while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+    {
+        if (msg.message == WM_QUIT)
+            return false;
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+    return true;
+}
+
+LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        return 0;
+    }
+    return DefWindowProcW(hwnd, msg, wp, lp);
+}
+
+} // namespace SE

Game/src/main.cpp
@@ -4,11 +4,16 @@
 int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
                    LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
 {
-    SE::Engine engine;
+    SE::WindowDesc desc;
+    desc.title  = L"FoxEngine";
+    desc.width  = 1280;
+    desc.height = 720;
 
-    if (!engine.Initialize())
+    SE::Engine engine;
+    if (!engine.Initialize(desc))
         return 1;
 
+    engine.Run();
     engine.Shutdown();
     return 0;
 }