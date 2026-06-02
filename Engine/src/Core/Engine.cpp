Foxtrot47/slopengiine

M04: Clock — frame delta, total elapsed, fixed-timestep accumulator
Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

Engine/src/Core/Engine.cpp
@@ -1,10 +1,12 @@
 #include "Engine/Core/Engine.h"
+#include <cstdio>
 
 namespace SE {
 
 bool Engine::Initialize(const WindowDesc& windowDesc)
 {
     Logger::Get().Initialize("slopengine.log");
+    m_clock.Initialize();
 
     if (!m_window.Open(windowDesc))
     {
@@ -19,11 +21,29 @@ bool Engine::Initialize(const WindowDesc& windowDesc)
 void Engine::Run()
 {
     SE_LOG_INFO("Entering main loop");
+
+    float fpsTimer = 0.0f;
+
     while (m_window.PumpMessages())
     {
+        m_clock.Tick();
         OnUpdate();
+
+        // Update title bar with FPS once per second.
+        fpsTimer += m_clock.GetDeltaTime();
+        if (fpsTimer >= 1.0f)
+        {
+            wchar_t title[128];
+            swprintf_s(title, L"SlopEngine  |  %.1f fps  |  %.2f ms",
+                       m_clock.GetFPS(),
+                       m_clock.GetDeltaTime() * 1000.0f);
+            SetWindowTextW(m_window.GetHandle(), title);
+            fpsTimer = 0.0f;
+        }
     }
-    SE_LOG_INFO("Exiting main loop");
+
+    SE_LOG_INFO("Exiting main loop — %llu frames, %.2fs total",
+                m_clock.GetFrameCount(), m_clock.GetTotalTime());
 }
 
 void Engine::Shutdown()

Foxtrot47/FoxEngine

Engine/src/Core/Engine.cpp
@@ -1,10 +1,12 @@
 #include "Engine/Core/Engine.h"
+#include <cstdio>
 
 namespace SE {
 
 bool Engine::Initialize(const WindowDesc& windowDesc)
 {
     Logger::Get().Initialize("FoxEngine.log");
+    m_clock.Initialize();
 
     if (!m_window.Open(windowDesc))
     {
@@ -19,11 +21,29 @@ bool Engine::Initialize(const WindowDesc& windowDesc)
 void Engine::Run()
 {
     SE_LOG_INFO("Entering main loop");
+
+    float fpsTimer = 0.0f;
+
     while (m_window.PumpMessages())
     {
+        m_clock.Tick();
         OnUpdate();
+
+        // Update title bar with FPS once per second.
+        fpsTimer += m_clock.GetDeltaTime();
+        if (fpsTimer >= 1.0f)
+        {
+            wchar_t title[128];
+            swprintf_s(title, L"FoxEngine  |  %.1f fps  |  %.2f ms",
+                       m_clock.GetFPS(),
+                       m_clock.GetDeltaTime() * 1000.0f);
+            SetWindowTextW(m_window.GetHandle(), title);
+            fpsTimer = 0.0f;
+        }
     }
-    SE_LOG_INFO("Exiting main loop");
+
+    SE_LOG_INFO("Exiting main loop — %llu frames, %.2fs total",
+                m_clock.GetFrameCount(), m_clock.GetTotalTime());
 }
 
 void Engine::Shutdown()